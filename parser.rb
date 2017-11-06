f45e32a1e523ae2d5305d6ff84071bb43cdc07b1
Gauthier POGAM--LE MONTAGNER <kendos.kenlen@gmail.com>
2017-11-06 14:48:08 +0900

    Avoid duplicate adding

--- parser.rb
+++ parser.rb
@@ -5,9 +5,9 @@ class Parser
     graph = Graph.new
 
     File.open(file).readlines.each_with_index do |line, line_index|
-      line.split(",").each_with_index do |distance, edge_index|
+      line.split(",").drop(line_index + 1).each_with_index do |distance, edge_index|
         graph.add?(edge_index) if line_index == 0
-        graph.add_link(line_index, edge_index, distance)
+        graph.add_link(line_index, edge_index, distance) unless line_index == edge_index
       end
     end
 
