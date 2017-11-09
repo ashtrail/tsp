7dfdf2d6019dbeab0d85959af21329c831aee244
ashtrail <arthur.cendrier@epitech.eu>
2017-11-09 15:53:57 +0900

    updated code to match current state

--- src/random_solver.rb
+++ src/random_solver.rb
@@ -1,13 +1,12 @@
 require_relative 'graph'
+require_relative 'utility'
 
 # finds a random solution for tsp
 class RandomSolver
 
   def solve(graph)
     solution = graph.get_node(0).edges.keys.shuffle
-    solution.unshift(0)
-    solution.push(0)
-    solution
+    Utility.reformat(solution)
   end
 
 end
